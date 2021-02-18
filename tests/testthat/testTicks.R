e3c9f8d306a124ade25d188088bdc71425d3bd2a
yurymaisiayonak <mys.1997mail@gmail.com>
2021-02-18 13:37:09 +0000

    exclude check is.data.table from test

--- tests/testthat/testTicks.R
+++ tests/testthat/testTicks.R
@@ -7,7 +7,7 @@ test_that("Is Last 10 Ticks right format", {
   }
   ticks <- ttPublicClient$GetTicksFromWeb("EURUSD", reqTimeInMS, count = -10)
   expect_equal(typeof(ticks), "list")
-  expect_true(is.data.table(ticks))
+  # expect_true(is.data.table(ticks))
   ticksColNames <- colnames(ticks)
   expect_identical(ticksColNames, c("Timestamp", "BidPrice", "BidVolume", "BidType","AskPrice","AskVolume", "AskType"))
 
@@ -21,7 +21,7 @@ test_that("Is Last 10 Bars right format", {
   }
   bars <- ttPublicClient$GetBarFromWeb("EURUSD", "Bid", "M1", reqTimeInMS, count = -10)
   expect_equal(typeof(bars), "list")
-  expect_true(is.data.table(bars))
+  # expect_true(is.data.table(bars))
   barsColNames <- colnames(bars)
   expect_identical(barsColNames, c("Volume", "Close", "Low", "High", "Open", "Timestamp"))
 })
@@ -31,11 +31,11 @@ test_that("Is Last 10 Bars right format", {
   if(requireNamespace("lubridate", quietly = TRUE)){
     reqTime <- lubridate::now("UTC")
   }else{
-    reqTimeInMS <- 0
+    reqTime <- 0
   }
   bars <- rhost$GetBarsHistory("EURUSD", "Bid", "M1", reqTime, reqTime, count = -10)
   expect_equal(typeof(bars), "list")
-  expect_true(is.data.table(bars))
+  # expect_true(is.data.table(bars))
   barsColNames <- colnames(bars)
   expect_identical(barsColNames, c("Volume", "Close", "Low", "High", "Open", "Timestamp"))
 })
@@ -44,11 +44,11 @@ test_that("Is Last 10 ticks right format", {
   if(requireNamespace("lubridate", quietly = TRUE)){
     reqTime <- lubridate::now("UTC")
   }else{
-    reqTimeInMS <- 0
+    reqTime <- 0
   }
   ticks <- rhost$GetTickHistory("EURUSD", reqTime, reqTime, count = -10)
   expect_equal(typeof(ticks), "list")
-  expect_true(is.data.table(ticks))
+  # expect_true(is.data.table(ticks))
   ticksColNames <- colnames(ticks)
   expect_identical(ticksColNames, c("Timestamp", "BidPrice", "BidVolume", "BidType","AskPrice","AskVolume", "AskType"))
 })
